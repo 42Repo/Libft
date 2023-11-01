a1124e1072a006ff4743afc893827205991916cf
Mael Buchs <mael@buchs.fr>
2023-11-01 18:38:43 +0100

    itoa v1000

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mbuchs <mael@buchs.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 23:28:48 by asuc              #+#    #+#             */
-/*   Updated: 2023/11/01 18:34:55 by mbuchs           ###   ########.fr       */
+/*   Updated: 2023/11/01 18:38:35 by mbuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static	char* neg_nbr(unsigned int n, int len)
 {
 	char	*str;
 
-	str = ft_calloc((len + 2), sizeof(char));
+	str = ft_calloc((len + 1), sizeof(char));
 		if(str == NULL)
 		return (NULL);
 	str[0] = '-';
@@ -58,13 +58,13 @@ char	*ft_itoa(int n)
 	len = ft_numlen(n);
 	if (n < 0)
 	{
-		str = ft_calloc((len + 2), sizeof(char));
+		str = ft_calloc((len + 1), sizeof(char));
 			if(str == NULL)
 				return (NULL);
 		str = neg_nbr(-n, len);
 		return (str);
 	}
-	str = ft_calloc((len + 2), sizeof(char));
+	str = ft_calloc((len + 1), sizeof(char));
 	if(str == NULL)
 		return (NULL);
 	len--;
