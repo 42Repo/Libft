3560381410837669252930be0945aeae05d74128
Mael Buchs <mael@buchs.fr>
2023-11-01 18:35:32 +0100

    itoa v1000

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mbuchs <mael@buchs.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 23:28:48 by asuc              #+#    #+#             */
-/*   Updated: 2023/11/01 18:21:23 by mbuchs           ###   ########.fr       */
+/*   Updated: 2023/11/01 18:34:55 by mbuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,18 @@ static int	ft_numlen(long long int n)
 	return (len);
 }
 
-static	char* neg_nr(long long int n, int len)
+static	char* neg_nbr(unsigned int n, int len)
 {
 	char	*str;
 
-	str = malloc((len + 1) * sizeof(char));
+	str = ft_calloc((len + 2), sizeof(char));
+		if(str == NULL)
+		return (NULL);
 	str[0] = '-';
 	len--;
 	while (len > 0)
 	{
 		str[len] = n % 10 + '0';
-		printf("%d", len);
 		n /= 10;
 		len--;
 	}
@@ -53,23 +54,24 @@ char	*ft_itoa(int n)
 {
 	int		len;
 	char	*str;
-	long long int nb;
 
-	nb = n;
-	len = ft_numlen(nb);
-	if (nb < 0)
+	len = ft_numlen(n);
+	if (n < 0)
 	{
-		str = malloc((len + 2) * sizeof(char));
-		str = neg_nr(-nb, len);
+		str = ft_calloc((len + 2), sizeof(char));
+			if(str == NULL)
+				return (NULL);
+		str = neg_nbr(-n, len);
 		return (str);
 	}
-	str = malloc((len +1) * sizeof(char));
+	str = ft_calloc((len + 2), sizeof(char));
+	if(str == NULL)
+		return (NULL);
 	len--;
 	while (len >= 0)
 	{
-		str[len] = nb % 10 + '0';
-		printf("%d", len);
-		nb /= 10;
+		str[len] = n % 10 + '0';
+		n /= 10;
 		len--;
 	}
 	return (str);
